dd4e308a5c70683cf804e0902ae9e900ca9672e8
Harry <09hareks@skola.boras.se>
2026-03-19 14:02:09 +0100

    pqformel added

--- pqchu.py
+++ pqchu.py
@@ -67,7 +67,13 @@ class PQApp(tk.Frame):
     
     def solve(self):
         left_poly, right_poly = self.read_fields()
-        self.answertext.set("The Solve button was pressed.")
+        left_poly = left_poly - right_poly
+        right_poly = 0
+        left_poly = left_poly * left_poly.a**-1
+        x1 = -left_poly.b / 2 + (left_poly.b/2 **2 - left_poly.c)**0.5
+        x2 = -left_poly.b / 2 - (left_poly.b/2 **2 - left_poly.c)**0.5
+        
+        self.answertext.set(str(x1,x2))
 
         ##NEEDS CODE TO SOLVE EQUATION HERE. CAN YOU DO IT?
 
